e5ec4cda3425be6f01129077891f9e37219909ee
Andrew Ketsko <monicman82@gmail.com>
2024-10-16 13:26:08 +0300

    fix: errors in record & phone

--- phone_class.py
+++ phone_class.py
@@ -3,5 +3,7 @@ from field_class import Field
 
 class Phone(Field):
     def __init__(self, value):
-        assert len(value)>=10, f'phone number must be greater than 10 digit'
+        if len(value)!=10:
+            raise ValueError('phone number must be greater than 10 digit')
+        # assert len(value)!=10, f'phone number must be greater than 10 digit'
         super().__init__(value)
--- record_class.py
+++ record_class.py
@@ -8,28 +8,35 @@ class Record:
         self.phones=[]
 
     # реалізація класу
+    def __phonelist_from_instancelist(self):
+        return [phone.value for phone in self.phones]
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
     
     def add_phone(self, phone_str:str)->None:
         phone=Phone(phone_str)
-        if phone not in self.phones:
+        if phone_str not in self.__phonelist_from_instancelist():
             self.phones.append(phone)
 
     def remove_phone(self, phone_str:str)->None:
         phone=Phone(phone_str)
         try:
-            self.phones.remove(phone)
+            index=self.__phonelist_from_instancelist().index(phone_str) #raise ValueError if not found
+            self.phones.pop(index)
         except ValueError:
             pass
 
     def find_phone(self,phone_str:str)->Phone|None:
         phone=Phone(phone_str) 
-        return phone_str if phone_str in [phone.value for phone in self.phones] else None
+        try:
+            index=self.__phonelist_from_instancelist().index(phone_str) #raise ValueError if not found
+            return self.phones[index] 
+        except ValueError:
+            return None
 
     def edit_phone(self, old_phone_str:str, new_phone_str:str)->None:
         old_phone=Phone(old_phone_str)
         new_phone=Phone(new_phone_str)
-        index=[phone.value for phone in self.phones].index(old_phone_str) #raise ValueError if not found
+        index=self.__phonelist_from_instancelist().index(old_phone_str) #raise ValueError if not found
         self.phones[index]=new_phone
